aab66be17c2b48ffe085c12238adc3afbe772e55
wakio-ngari <ngarijoyce017@gmail.com>
2024-11-28 17:30:55 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function returnCustomerName(){
+    return customerName;
+}
+console.log(customerName);
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+
+}
+var bestcustomer;
+
+function setBestCustomer(){
+    bestCustomer ='not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer){
+    bestCustomer='maybe bob';
+
+}
+
+const leastFavoriteCustomer ='initial customer';
+
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer='new customer';
+ }
+ 
+ 
+
+
